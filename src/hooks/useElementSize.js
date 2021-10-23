a7e8ca7cc808e56d1f1b77947fb0f6867eb72549
leau-void <lpierra@gmail.com>
2021-10-23 18:54:41 -0300

    useElementSize simpler, only one useEffect and initial size gets done in useEffect

--- src/hooks/useElementSize.js
+++ src/hooks/useElementSize.js
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from "react";
 
 const getSize = (elem, pixelRatio) => ({
-  height: elem.getBoundingClientRect().height * pixelRatio,
-  width: elem.getBoundingClientRect().width * pixelRatio,
+  height: elem.offsetHeight * pixelRatio,
+  width: elem.offsetWidth * pixelRatio,
 });
 
 const useElementSize = (ref) => {
-  const [size, setSize] = useState({});
+  const [size, setSize] = useState();
 
   const [pixelRatio, setPixelRatio] = useState(window.devicePixelRatio);
 
-  useEffect(() => {
-    setSize(getSize(ref.current, pixelRatio));
-  }, []);
+  const handleResize = (e) => {
+    const newPixelRatio = window.devicePixelRatio;
+    if (ref.current && pixelRatio === newPixelRatio) {
+      setSize(getSize(ref.current, pixelRatio));
+    } else {
+      setPixelRatio(newPixelRatio);
+    }
+  };
 
   useEffect(() => {
-    const handleResize = (e) => {
-      const newPixelRatio = window.devicePixelRatio;
-      if (ref.current && pixelRatio === newPixelRatio) {
-        setSize(getSize(ref.current, pixelRatio));
-      } else {
-        setPixelRatio(newPixelRatio);
-      }
-    };
+    // initial size when ref is available
+    if (!size) setSize(getSize(ref.current, pixelRatio));
+
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, [ref]);
